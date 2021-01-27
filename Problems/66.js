bfcef37f2746cbd07e0c88bb05acba70d94ae0e4
daniel <daniel@rical.net>
2021-01-28 01:05:09 +0800

    [feat] add problem 66

---
+++ Problems/66.js
@@ -0,0 +1,23 @@
+// 解題思路：若該位數加一後為十，則向前 (左) 進位，若已是最左邊的位置時，則 unshift 新增一位數
+var plusOne = function (digits) {
+  // plus one
+  digits[digits.length - 1] += 1;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 10) {
+      digits[i] = 0;
+
+      if (i > 0) digits[i - 1] += 1;
+      else digits.unshift(1);
+    }
+  }
+
+  return digits;
+};
+
+module.exports = {
+  title: "Plus One",
+  difficulty: "easy",
+  related_topics: { Array: true },
+  similar_questions: { 43: true, 67: true, 369: true, 989: true },
+};
--- Problems/index.js
+++ Problems/index.js
@@ -1,7 +1,9 @@
+const probelm_66 = require("src/Problems/66");
 const probelm_231 = require("src/Problems/231");
 const probelm_412 = require("src/Problems/412");
 
 module.exports = {
+  66: probelm_66,
   231: probelm_231,
   412: probelm_412,
 };
