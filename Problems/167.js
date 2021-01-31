d39215236c7773a7684735abe5d1842beab3bbc6
daniel <daniel@rical.net>
2021-02-01 00:12:23 +0800

    [feat] add problem 167

---
+++ Problems/167.js
@@ -0,0 +1,37 @@
+// 解題思路：第一次解題時，以為跟 problem_1 一樣，用 One-pass Hash Table 的解法
+var twoSum = function (nums, target) {
+  const numsMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numsMap.hasOwnProperty(target - nums[i])) {
+      return [numsMap[target - nums[i]], i];
+    } else {
+      numsMap[nums[i]] = i;
+    }
+  }
+};
+
+// 解題思路：後來發現可以利用 Two Pointers 的方式，左右夾擊，
+//          若兩個 pointer 相加比目標數值小時，則左邊指標往右，
+//          反之，則右邊指標往左。
+var twoSum = function (nums, target) {
+  let rightPointer = nums.length - 1;
+  let leftPointer = 0;
+
+  while (rightPointer > leftPointer) {
+    if (nums[rightPointer] + nums[leftPointer] > target) {
+      rightPointer -= 1;
+    } else if (nums[rightPointer] + nums[leftPointer] < target) {
+      leftPointer += 1;
+    } else {
+      return [leftPointer + 1, rightPointer + 1];
+    }
+  }
+};
+
+module.exports = {
+  title: "Two Sum II - Input array is sorted",
+  difficulty: "easy",
+  related_topics: { Array: true, "Two Pointers": true, "Binary Search": true },
+  similar_questions: { 1: true, 653: true, 1099: true },
+};
--- Problems/index.js
+++ Problems/index.js
@@ -1,11 +1,13 @@
 const probelm_1 = require("src/Problems/1");
 const probelm_66 = require("src/Problems/66");
+const probelm_167 = require("src/Problems/167");
 const probelm_231 = require("src/Problems/231");
 const probelm_412 = require("src/Problems/412");
 
 module.exports = {
   1: probelm_1,
   66: probelm_66,
+  167: probelm_167,
   231: probelm_231,
   412: probelm_412,
 };
